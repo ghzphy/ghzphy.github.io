581364ae3efbd9859ad0ae18dabe004990fde00e
ghzphy <ghzphysics@163.com>
2020-07-15 23:25:29 +0800

    modified

--- CS/GitBook/xiugai.py
+++
@@ -1,7 +0,0 @@
-with open('./gitbook/theme.js','r',encoding='utf-8') as f:
-	content = f.read()
-print(content)
-content = content.replace('f(m)','f(false)')
-
-with open('./gitbook/theme.js','w',encoding='utf-8') as f:
-	f.write(content)
